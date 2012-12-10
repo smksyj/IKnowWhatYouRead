b2d5b4c6489d638ce956d7e100f9cb7a0fa33c98
Seo Min-kwan <smksyj@naver.com>
2012-12-10 23:14:10 +0900

    커밋

--- src/org/cauoop/filter/ArticleFilter.java
+++ src/org/cauoop/filter/ArticleFilter.java
@@ -6,7 +6,7 @@ public class ArticleFilter {
 	
 	public ArticleFilter(String word,String line){
 		category = word;
-		split = line.replaceAll("(\\*|@|#|\\$|_|&|\\!|\\%|\\.|\\,|\\(.*\\)|\\>|-)", "").replace("를 ", " ").replace("는 ", " ").replace("이 ", " ").replace("가 ", " ").replace("은 ", " ").replace("에서 ", " ").replace("을 ", " ").replace("라고 ", " ").replace("라 ", " ").replace("과 ", " ").replace("에 ", " ").replace("하고 ", " ").replace("하는 ", " ").replace("으로 ", " ").split(" ");
+		split = line.replaceAll("(\\*|@|#|\\$|_|&|\\!|\\%|\\.|\\,|\\(.*\\)|\\>|-|[Aa][Ll][Ll])", "").replace("를 ", " ").replace("는 ", " ").replace("이 ", " ").replace("가 ", " ").replace("은 ", " ").replace("에서 ", " ").replace("을 ", " ").replace("라고 ", " ").replace("라 ", " ").replace("과 ", " ").replace("에 ", " ").replace("하고 ", " ").replace("하는 ", " ").replace("으로 ", " ").split(" ");
 	}
 	
 	public String getCategory(){
